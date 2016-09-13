221fe10ec44d583ed923725d5b802fa51aa80396
Eddy Verbruggen <eddyverbruggen@gmail.com>
2016-09-13 18:41:19 +0000

    demo app

--- GoogleSignIn/scripts/demo.js
+++ GoogleSignIn/scripts/demo.js
@@ -3,113 +3,86 @@
       geoTransitionListener = null,
       app = global.app = global.app || {};
   
-  // TODO
-  
   DemoViewModel = kendo.data.ObservableObject.extend({
     
-    initGeo: function () {
+    login: function () {
       if (!this.checkSimulator()) {
-        geofence.initialize(this.onSuccessWithAlert);
+        var feedback = document.querySelector("#feedback");
+        window.plugins.googleplus.login(
+          {},
+          function (obj) {
+            feedback.innerHTML = "Hi, " + obj.displayName + ", " + obj.email;
+
+            var image = document.querySelector("#image");
+            image.src = obj.imageUrl;
+            image.style.visibility = 'visible';
+          },
+          function (msg) {
+            feedback.innerHTML = "error: " + msg;
+          }
+        );
+      }
+    },
+
+    trySilentLogin: function () {
+      if (!this.checkSimulator()) {
+        var feedback = document.querySelector("#feedback");
+        window.plugins.googleplus.trySilentLogin(
+          {},
+          function (obj) {
+            feedback.innerHTML = "(Silent) Hi, " + obj.displayName + ", " + obj.email;
+
+            var image = document.querySelector("#image");
+            image.src = obj.imageUrl;
+            image.style.visibility = 'visible';
+          },
+          function (msg) {
+            feedback.innerHTML = "error: " + msg;
+          }
+        );
       }
     },
     
-    monitorEnterLeave20m: function () {
-      this._determineLocation('20m', 20);
-    },
-
-    monitorEnterLeave500m: function () {
-      this._determineLocation('500m', 500);      
-    },
-
-    _determineLocation: function (id, radius) {
-      var that = this;
-      navigator.geolocation.getCurrentPosition(
-        function(position) {
-          that._monitorEnterLeave(id, radius, position)
-        },
-        that.onError
-      );
-    },
-
-    _monitorEnterLeave: function (id, radius, position) {
-      var that = this;
-      geofence.addOrUpdate ({
-          id:             id, //A unique identifier of geofence
-          latitude:       position.coords.latitude, //Geo latitude of geofence
-          longitude:      position.coords.longitude, //Geo longitude of geofence
-          radius:         radius, //Radius of geofence in meters
-          transitionType: 3, //Type of transition 1 - Enter, 2 - Exit, 3 - Both
-          notification: {         //Notification object
-              id:             1, //optional should be integer, id of notification
-              title:          'Geofencing enter/leave', //Title of notification
-              text:           'For id: ' + id + ', radius: ' + radius, //Text of notification
-//              smallIcon:      String, //Small icon showed in notification area, only res URI
-//              icon:           String, //icon showed in notification drawer
-              openAppOnClick: true,//is main app activity should be opened after clicking on notification
-//              vibration:      [Integer], //Optional vibration pattern - see description
-              data:           {id: id}  //Custom object associated with notification
+    logout: function () {
+      if (!this.checkSimulator()) {
+        var feedback = document.querySelector("#feedback");
+        window.plugins.googleplus.logout(
+          function (msg) {
+            feedback.innerHTML = msg;
+            document.querySelector("#image").style.visibility = 'hidden';
+          },
+          function (msg) {
+            feedback.innerHTML = "error: " + msg;
           }
-      }).then(
-        function () {
-          // now that we have a geofence we should listen for transitions,
-          // so set up a listener if we don't have one already
-          if (geoTransitionListener === null) {
-            geoTransitionListener = function (geofences) {
-              geofences.forEach(function (geo) {
-                that.onSuccessWithAlert('Geofence transition detected', JSON.stringify(geo));
-              });
-            };
-            geofence.onTransitionReceived = geoTransitionListener;
-          };
-	        that.onSuccessWithAlert("Geofence added");
-        },
-        that.onError
-      );
+        );
+      }
     },
 
-    remove20m: function () {
-      geofence.remove('20m').then(this.onSuccessWithAlert);
-    },
-
-    remove500m: function () {
-      geofence.remove('500m').then(this.onSuccessWithAlert);
-    },
-
-    removeAll: function () {
-      geofence.removeAll().then(this.onSuccessWithAlert);
+    disconnect: function () {
+      if (!this.checkSimulator()) {
+        var feedback = document.querySelector("#feedback");
+        window.plugins.googleplus.disconnect(
+          function (msg) {
+            feedback.innerHTML = msg;
+            document.querySelector("#image").style.visibility = 'hidden';
+          },
+          function (msg) {
+            feedback.innerHTML = "error: " + msg;
+          }
+        );
+      }
     },
 
     checkSimulator: function() {
       if (window.navigator.simulator === true) {
         alert('This plugin is not available in the simulator.');
         return true;
-      } else if (window.geofence === undefined) {
+      } else if (window.plugins === undefined || window.plugins.googleplus === undefined) {
         alert('Plugin not found. Maybe you are running in AppBuilder Companion app which currently does not support this plugin.');
         return true;
       } else {
         return false;
       }
-    },
-
-    // callbacks
-    onSuccess: function(msg) {
-      if (msg === undefined) {
-        console.log('OK, done');
-      } else {
-	      console.log('OK, details: ' + JSON.stringify(msg));        
-      }
-    },
-
-    onSuccessWithAlert: function(msg) {
-      if (msg === undefined || msg === null) {
-        alert('OK, done');
-      } else {
-	      alert('Success message: ' + JSON.stringify(msg));        
-      }
-    },
-
-    onError: function(msg) {
-      alert('Geofencing error: ' + JSON.stringify(msg));
     }
   });
 
